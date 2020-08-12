6094ee7e69e7aa639b011f18a70af1b6ec5f7c43
lasindu-demel <lasindudemel@gmail.com>
2020-08-12 17:48:55 +0530

    first commit

---
+++ controller.js
@@ -0,0 +1,72 @@
+const pool = require('./db')
+const bodyParser = require('body-parser')
+const express = require('express')
+const { req, res } = require('express')
+
+//const app = express()
+
+
+const home =  (req, res) => {
+    res.json({info:'Hi, Good to go'})
+}
+
+//Get all users
+const getUsers = (req, res) =>{
+    pool.query('select * from users order by id',(error,results) => {
+        if(error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+}
+//Get userById
+const getUserById = (req,res) => {
+    const id = parseInt(req.params.id)
+    pool.query('select * from users where id = $1',[id],(error,results) =>{
+        if (error) {
+            throw error
+        }
+         res.status(200).json(results.rows)
+    })
+}
+//Post create user
+const createUser = (req,res) => {
+    const {name,email} = req.body
+    pool.query('insert into users(name,email) values ($1,$2) returning *',[name,email],(error,results) => {
+        if(error){
+            throw error
+        }
+        res.status(201).json(results.rows)
+    })
+}
+
+//Put update user
+const updateUser = (req,res) => {
+    const id = parseInt(req.params.id)
+    const {name,email} = req.body
+    pool.query('update users set name = $1 , email = $2 where id = $3 returning *',[name,email,id],(error,results) => {
+        if(error){
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+}
+//Delete userById
+const deleteUser = (req,res) => {
+    const id = parseInt(req.params.id)
+    pool.query('delete from users where id = $1 returning *',[id],(error,results) => {
+        if(error){
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+}
+
+module.exports ={
+    home,
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+const controller = require('./controller')
+const express = require('express')
+const bodyParser = require('body-parser')
+// const { response } = require('express')
+const app = express()
+const port = 3001
+
+app.use(bodyParser.json())
+app.use( bodyParser.urlencoded({ extended: true,}))
+
+
+app.get('/',controller.home)
+app.get('/users',controller.getUsers)
+app.get('/users/:id', controller.getUserById)
+app.post('/user/create',controller.createUser)
+app.put('/user/update/:id',controller.updateUser)
+app.delete('/user/delete/:id',controller.deleteUser)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
+
