1ba5a937fac0046258fb6fa3c013a0dc32d8761b
lasindu-demel <lasindudemel@gmail.com>
2020-08-14 17:00:01 +0530

    port change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 // const { response } = require('express')
 const { body, validationResult } = require('express-validator');
 const app = express()
-const port = 3001
+const port = 3000
 
 app.use(bodyParser.json())
 app.use( bodyParser.urlencoded({ extended: true,}))
